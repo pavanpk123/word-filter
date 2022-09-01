dca98496e69d39438bcf9d91e3500b5f9288459c
pavan <Pavanpk123@gmail.com>
2022-09-01 18:58:55 +0530

    word filter

---
+++ index.js
@@ -0,0 +1,10 @@
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var results = numbers.filter(number=>number>5);
+console.log(results)
+
+
+// words filters 
+
+var words = ["pavan","kalyan","kumar","vinod"];
+var result = words.filter(word=>word.lenth>4);
+console.log(result)
